7c29586635078c27cee509bef07c5045ce97aba6
James Tran <tran.james2001@gmail.com>
2022-09-22 20:56:17 -0400

    Parse queue message body

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import json
+
 import boto3
 
 sqs = boto3.resource("sqs")
-
 queue = sqs.get_queue_by_name(QueueName="inference-request.fifo")
-messages = queue.receive_messages()
-for m in messages:
-    print(m.body)
-    m.delete()
+
+messages = queue.receive_messages(WaitTimeSeconds=20, MaxNumberOfMessages=1)
+for message in messages:
+    body = json.loads(message.body)
+
+    message.delete()
